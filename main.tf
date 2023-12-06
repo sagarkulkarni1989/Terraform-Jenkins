ca568f057ab62dd1931b3a0fd286b9dc54429b6e
Sagar Kulkarni <46215433+sagarkulkarni1989@users.noreply.github.com>
2023-12-06 12:25:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region = "ap-south-1"  
+    region = "us-east-2"  
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-02a2af70a66af6dfb" # us-west-2
+  ami           = "ami-06d4b7182ac3480fa" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
